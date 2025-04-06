eb1e94342c61aadff22f525b91daca7b69db3b9f
yedukondalu45 <kelaru349@gmail.com>
2025-04-06 12:39:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,5 @@ document.getElementById('contact-form').addEventListener('submit', function (e) 
   alert('Thank you for your message! We will get back to you soon.');
   this.reset();
 });
+
+hiiiiiii
